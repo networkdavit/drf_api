e22e043841d1e0760d6fe1acf1b2fe70771baeb2
networkdavit <lonecreation1612@gmail.com>
2023-02-15 02:02:04 +0400

    Adding new endpoint to check if logged in

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import RegisterView
+from authentication.views import RegisterView, VerifyTokenView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
+    path('verify-token/', VerifyTokenView.as_view(), name='verify_token'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,9 +2,28 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenVerifySerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class VerifyTokenView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        token = request.data.get('token') or request.headers.get('Authorization').split(' ')[1]
+        # If token is in the request body, use the line above instead of the one below
+        # token = request.data.get('token')
+        serializer = TokenVerifySerializer(data={'token': token})
+        try:
+            serializer.is_valid(raise_exception=True)
+            return Response({'valid': True})
+        except:
+            return Response({'valid': False})
