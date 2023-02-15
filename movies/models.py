4ddd9c10646e9d0a9786c464b50a0e7b68b29707
networkdavit <lonecreation1612@gmail.com>
2023-02-15 20:23:19 +0400

    Migrations

--- movies/models.py
+++ movies/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    imageUrl = models.CharField(max_length=300)
     year = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
